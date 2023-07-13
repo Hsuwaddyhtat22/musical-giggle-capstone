b8f645ca29510ce620d2beba1b91e813d27b1953
Hsu Waddy <hsuwady2001@gmail.com>
2023-07-13 22:15:05 +0630

    Deploye-add-dot

--- index.js
+++ index.js
@@ -12,27 +12,27 @@ document.getElementById("menu-two").addEventListener("click", function() {
 });
 
 const cardDatas = [{
-        icon: 'images/card1.png',
+        icon: './images/card1.png',
         title: 'Lecture',
         description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quidem, veniam enim? Corporis quisquam quos obcaecati minus consectetur ratione iusto veritatis nam, illo quibusdam itaque aut, dolorum undeculpa delectus.Necessitatibum.',
     },
     {
-        icon: '/images/card2.png',
+        icon: './images/card2.png',
         title: 'CC Exhibition',
         description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quidem, veniam enim? Corporis quisquam quos obcaecati minus consectetur ratione iusto veritatis nam, illo quibusdam itaque aut, dolorum undeculpa delectus.Necessitatibum.',
     },
     {
-        icon: '/images/card3.png',
+        icon: './images/card3.png',
         title: 'Forum',
         description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quidem, veniam enim? Corporis quisquam quos obcaecati minus consectetur ratione iusto veritatis nam, illo quibusdam itaque aut, dolorum undeculpa delectus.Necessitatibum.',
     },
     {
-        icon: '/images/card4.png',
+        icon: './images/card4.png',
         title: 'Workshop',
         description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quidem, veniam enim? Corporis quisquam quos obcaecati minus consectetur ratione iusto veritatis nam, illo quibusdam itaque aut, dolorum undeculpa delectus.Necessitatibum.',
     },
     {
-        icon: '/images/card5.png',
+        icon: './images/card5.png',
         title: 'CC Ignite',
         description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quidem, veniam enim? Corporis quisquam quos obcaecati minus consectetur ratione iusto veritatis nam, illo quibusdam itaque aut, dolorum undeculpa delectus.Necessitatibum.',
     }
